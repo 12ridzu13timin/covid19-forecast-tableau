945df3c757c413d696be1b9f276c078c7a76eb13
12ridzu13timin <121460080+12ridzu13timin@users.noreply.github.com>
2025-08-11 00:35:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import streamlit as st
 
 st.set_page_config(page_title="My Tableau Dashboard", layout="wide")
 
-st.title("COVID-19 Dashboard (Tableau + Streamlit)")
+st.title("COVID-19 Dashboard 2020-2022")
 
 st.markdown("""
-This dashboard is powered by Tableau Public and embedded in Streamlit.
+MC - O31
+ITIB4114-BUSINESS INTELLIGENCE
+MAY 2025
+GROUP 5
 """)
 
-# Replace this with your own Tableau Public embed link
 tableau_embed_code = """
 <iframe src="https://public.tableau.com/views/Book1_17548411159820/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link"
 width="100%" height="800px"></iframe>
